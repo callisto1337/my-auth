bfa6f77cfa8c64a286bd555010825112f937e3e7
callisto1337 <hazardous333@gmail.com>
2015-12-15 23:38:10 +0300

    upgrade

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var app = express();
 var cookieParser = require('cookie-parser');
 var jade = require('jade');
 var bodyParser = require('body-parser');
-var expressSession = require('express-session');
+var session = require('express-session');
 
 app.set('view engine', 'jade');
-app.use(expressSession({secret: 'mySecretKey'}));
 app.use(bodyParser());
-app.use(cookieParser());
+app.use(cookieParser("megaultrasupersecret"));
+app.use(session({secret: 'my secret'}));
 
 app.get('/', function(req, res){
     res.render('index');
 });
 
 app.get('/logged', function(req, res){
-  if (req.cookies.remember) {
+  if (req.cookies.cookie) {
     res.render('logged');
   } else {
     res.redirect('/');
@@ -25,14 +25,14 @@ app.get('/logged', function(req, res){
 app.post('/logged', function (req, res, next) {
     if (req.body.username == '123' && req.body.password == '321') {
     	res.cookie('cookie', 1337);
-        res.render('logged');
+        res.redirect('/logged');
     } else {
         res.redirect('/');
     }
 });
 
 app.post('/exit', function(req, res) {
-	res.clearCookie('cookie');
+	res.clearCookie('cookie', 1337);
 	res.redirect('/');
 })
 
